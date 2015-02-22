f500cc28345a43d6d6959541b5f87a2b76fc3953
gssiyankai <syk.greg@gmail.com>
2015-02-22 17:27:14 +0100

    [wip] split

--- utils.hpp
+++ utils.hpp
@@ -8,21 +8,45 @@ class Utils
 
 public:
     template <typename T>
-            static std::vector< std::pair<T,T> > combination2(const std::vector<T>& a);
+        static std::vector<
+            std::pair<
+                    std::vector<T> , std::vector<T> > > split(const std::vector<T>& a);
+
+private:
+    template <typename T>
+        static std::vector<
+            std::pair<
+                    std::vector<T> , std::vector<T> > > split(const std::vector<T>& a,
+                                                              std::vector<T> a1, std::vector<T> a2,
+                                                              unsigned int n_begin, unsigned int n_end,
+                                                              unsigned int iter, unsigned int size);
 
 };
 
 template <typename T>
-std::vector< std::pair<T,T> > Utils::combination2(const std::vector<T>& a)
+std::vector<
+        std::pair< std::vector<T>, std::vector<T> > > Utils::split(const std::vector<T>& a)
 {
-    std::vector< std::pair<T,T> > r;
-    for(int i = 0; i < a.size(); ++i)
+    std::vector< std::pair< std::vector<T>, std::vector<T> > > r;
+    for(int i = 1; i <= a.size()/2; ++i)
     {
-        for(int j = i+1; j<a.size(); ++j)
-        {
-            r.push_back(std::pair<T,T>(a[i], a[j]));
-        }
+        const std::vector< std::pair< std::vector<T>, std::vector<T> > >&  ri = split(a,
+                                                                                      std::vector<T>(), std::vector<T>(),
+                                                                                      0, a.size(), 0, i);
+        r.insert(r.end(), ri.begin(), ri.end());
     }
+    return r;
+}
+
+template <typename T>
+std::vector<
+        std::pair<
+                std::vector<T> , std::vector<T> > > Utils::split(const std::vector<T>& a,
+                                                                 std::vector<T> a1, std::vector<T> a2,
+                                                                 unsigned int n_begin, unsigned int n_end,
+                                                                 unsigned int iter, unsigned int size)
+{
+    std::vector< std::pair< std::vector<T>, std::vector<T> > > r;
     return r;
 }
 
--- utils_tests.cpp
+++ utils_tests.cpp
@@ -3,12 +3,32 @@
 
 using namespace std;
 
-TEST(UtilsTest, Combination2)
+TEST(UtilsTest, Split)
 {
-    const vector<int>& v = { 1, 2, 3 };
-    const vector< pair<int,int> >& r = Utils::combination2(v);
-    ASSERT_EQ(3, r.size());
-    ASSERT_EQ(make_pair(1,2), r[0]);
-    ASSERT_EQ(make_pair(1,3), r[1]);
-    ASSERT_EQ(make_pair(2,3), r[2]);
+    const vector<int> v = { 1, 2, 3 };
+    const vector< pair< vector<int>, vector<int> > >& expected = { pair< vector<int>, vector<int> >({1}, {2, 3}),
+                                                                   pair< vector<int>, vector<int> >({2}, {1, 3}),
+                                                                   pair< vector<int>, vector<int> >({3}, {1, 2}) };
+    ASSERT_EQ(expected, Utils::split(v));
+}
+
+TEST(UtilsTest, SplitMore)
+{
+    const vector<int> v = { 1, 2, 3, 4, 5 };
+    const vector< pair< vector<int>, vector<int> > >& expected = { pair< vector<int>, vector<int> >({1},    {2, 3, 4, 5}),
+                                                                   pair< vector<int>, vector<int> >({2},    {1, 3, 4, 5}),
+                                                                   pair< vector<int>, vector<int> >({3},    {1, 2, 4, 5}),
+                                                                   pair< vector<int>, vector<int> >({4},    {1, 2, 3, 5}),
+                                                                   pair< vector<int>, vector<int> >({5},    {1, 2, 3, 4}),
+                                                                   pair< vector<int>, vector<int> >({1, 2}, {3, 4, 5}),
+                                                                   pair< vector<int>, vector<int> >({1, 3}, {2, 4, 5}),
+                                                                   pair< vector<int>, vector<int> >({1, 4}, {2, 3, 5}),
+                                                                   pair< vector<int>, vector<int> >({1, 5}, {2, 3, 4}),
+                                                                   pair< vector<int>, vector<int> >({2, 3}, {1, 4, 5}),
+                                                                   pair< vector<int>, vector<int> >({2, 4}, {1, 3, 5}),
+                                                                   pair< vector<int>, vector<int> >({2, 5}, {1, 3, 4}),
+                                                                   pair< vector<int>, vector<int> >({3, 4}, {1, 2, 5}),
+                                                                   pair< vector<int>, vector<int> >({3, 5}, {1, 2, 4}),
+                                                                   pair< vector<int>, vector<int> >({4, 5}, {1, 2, 3}) };
+    ASSERT_EQ(expected, Utils::split(v));
 }
